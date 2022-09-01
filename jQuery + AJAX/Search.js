f1e75e5531dac39f8791ccd0ae0b26ff7d2ba2b0
LauriHe <Lauri.Heikkinen5@metropolia.fi>
2022-09-01 10:56:49 +0300

    improved search.js

--- jQuery + AJAX/Search.js
+++ jQuery + AJAX/Search.js
@@ -19,13 +19,15 @@ $('#searchForm').submit(function (event) {
     $.each(shows, function (i, show) {
       const name = show.show.name;
       const site = show.show.officialSite || show.show.url;
+      const genres = show.show.genres;
       const description = show.show.summary;
-      const image = show.show.image.medium;
+      const image = show.show.image?.medium || "https://static.tvmaze.com/images/no-img/no-img-portrait-text.png";
       $('#result').append(`
       <article>
-        Name: ${name} <br>
-        Site: <a href="${site}">link</a> <br>
-        Description: ${description} <br>
+        <div>Name: ${name}</div> <br>
+        <a href="${site}">Link to show</a> <br>
+        <div>Genres: ${genres.join(' | ')}</div>
+        <div>Description: ${description}</div> <br>
         <img src="${image}" alt="cover image for ${name}"> <br><br>
       </article>`);
     });
