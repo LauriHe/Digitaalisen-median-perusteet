749689641717355f43817e93db787b9a85a04900
LauriHe <Lauri.Heikkinen5@metropolia.fi>
2022-09-30 21:43:47 +0300

    Modified the appearance of jQuery + AJAX exercise with bootsrap

--- jQuery + AJAX/Search.js
+++ jQuery + AJAX/Search.js
@@ -21,15 +21,33 @@ $('#searchForm').submit(function (event) {
       const site = show.show.officialSite || show.show.url;
       const genres = show.show.genres;
       const description = show.show.summary;
-      const image = show.show.image?.medium || "https://static.tvmaze.com/images/no-img/no-img-portrait-text.png";
+      const image = show.show.image?.original || "https://static.tvmaze.com/images/no-img/no-img-portrait-text.png";
       $('#result').append(`
-      <article>
-        <div>Name: ${name}</div> <br>
-        <a href="${site}">Link to show</a> <br>
-        <div>Genres: ${genres.join(' | ')}</div>
-        <div>Description: ${description}</div> <br>
-        <img src="${image}" alt="cover image for ${name}"> <br><br>
-      </article>`);
+      <article class="row py-5">
+          <div class="row mb-3">
+            <h2>${name}</h2>
+          </div>
+          <div class="col-4">
+            <div class="row">
+              <img src="${image}" alt="cover image for ${name}">
+            </div>
+          </div>
+          <div class="col">
+            <div class="row pb-3">
+              <h3>Genres: ${genres.join(' | ')}</h3>
+            </div> 
+            <div class="row py-3">
+              <h3>Description</h3>
+              <div class"">${description}</div>
+            </div>
+            <div class="row py-3">
+              <div class="col-3">
+                <a class="btn btn-danger" href="${site}">Link to show</a>
+              </div>
+            </div>
+          </div>
+      </article>
+      `);
     });
   });
 
