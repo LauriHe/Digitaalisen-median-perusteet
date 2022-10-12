22729996022f4d4db54acdba2297a61df32d8cb1
LauriHe <Lauri.Heikkinen5@metropolia.fi>
2022-10-13 01:58:32 +0300

    Videos now pause when closing modal

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -10,6 +10,16 @@ const video = $("#video");
 const audio = $("#audio");
 const divider = $(".divider");
 
+const afterEffectsModal = $("#afterEffectsModal");
+const videosprintti1Modal = $("#videosprintti1Modal");
+const videosprintti2Modal = $("#videosprintti2Modal");
+const moniraitamiksausModal = $("#moniraitamiksausModal");
+
+const afterEffectsVideo = $("#afterEffectsVideo");
+const videosprintti1Video = $("#videosprintti1Video");
+const videosprintti2Video = $("#videosprintti2Video");
+const moniraitamiksausVideo = $("#moniraitamiksausVideo");
+
 const animationTime = 500;
 
 allLink.click(function() {
@@ -54,4 +64,20 @@ audioLink.click(function() {
   allLink.removeClass("active");
   webLink.removeClass("active");
   videoLink.removeClass("active");
+});
+
+afterEffectsModal.on('hidden.bs.modal', function (event){
+  afterEffectsVideo[0].pause();
+});
+
+videosprintti1Modal.on('hidden.bs.modal', function (event){
+  videosprintti1Video[0].pause();
+});
+
+videosprintti2Modal.on('hidden.bs.modal', function (event){
+  videosprintti2Video[0].pause();
+});
+
+moniraitamiksausModal.on('hidden.bs.modal', function (event){
+  moniraitamiksausVideo[0].pause();
 });
